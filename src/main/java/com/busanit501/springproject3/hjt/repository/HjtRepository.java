3940626cac8e2818b388a1467c97d0bb4ef29641
HongJintae <ghdwlsxo33@gmail.com>
2024-08-23 11:30:15 +0900

    두번째 커밋

--- src/main/java/com/busanit501/springproject3/hjt/repository/HjtRepository.java
+++ src/main/java/com/busanit501/springproject3/hjt/repository/HjtRepository.java
@@ -1,4 +1,8 @@
 package com.busanit501.springproject3.hjt.repository;
 
-public interface HjtRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HjtRepository extends JpaRepository{
 }
