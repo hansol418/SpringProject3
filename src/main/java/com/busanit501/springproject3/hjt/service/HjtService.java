15bc11a87e7c85a77e6a6f3ed6238fa02a4ef188
ghdwl <ghdwlsxo33@gmail.com>
2024-08-26 00:57:08 +0900

    네번째 커밋

--- src/main/java/com/busanit501/springproject3/hjt/service/HjtService.java
+++ src/main/java/com/busanit501/springproject3/hjt/service/HjtService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 @Service
@@ -20,29 +21,12 @@ public class HjtService {
     @Autowired
     private HjtRepository hjtRepository;
 
-    public void click() throws JsonProcessingException {
-        String apiUrl = "https://apis.data.go.kr/6260000/AttractionService/getAttractionKr";
-        String apiKey = "ViNhf9KFHrhlepP82G2riFCwzySQxL4juLIE5itFGrf8lpCixgdypSpsC930g7033hqAO8PKM99K5eNbt13uSA==";
-        String url = apiUrl + "?serviceKey=" + apiKey + "&pageNo=1&numOfRows=21";
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-        String response = responseEntity.getBody();
+    public List<HjtEntity> findAll() {
+        return hjtRepository.findAll();
+    }
 
-        XmlMapper mapper = new XmlMapper();
-
-        JsonNode root = mapper.readTree(response);
-        JsonNode itemsNode = root.path("body").path("items").path("item");
-
-        List<HjtEntity> hjtEntityList = new ArrayList<>();
-
-        for (JsonNode node : itemsNode) {
-            HjtEntity hjtEntity = HjtEntity.builder()
-                    .tool_name(node.path("MAIN_TITLE").asText(null))
-                    .description(node.path("ITEMCNTNTS").asText(null))
-                    .img_text(node.path("MAIN_IMG_THUMB").asText(null))
-                    .build();
-            hjtEntityList.add(hjtEntity);
-        }
-
+    public Optional<HjtEntity> findById(Long id) {
+        return hjtRepository.findById(id);
     }
 }
+
