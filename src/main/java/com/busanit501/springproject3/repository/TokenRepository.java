4f5ce3677fd24258a2dc8886e856c124d42ea7ac
LeeHanSol <hslee4954@gmail.com>
2024-08-27 14:50:15 +0900

    충돌을 방지하기위해 본인 이름 폴더로 캡슐화 작업

--- src/main/java/com/busanit501/springproject3/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.busanit501.springproject3.repository;
-
-
-import com.busanit501.springproject3.domain.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Long> {
-}
