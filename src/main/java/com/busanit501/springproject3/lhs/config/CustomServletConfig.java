7e40626ca3b5bcb9fee00f7aa95d1ce09870fe69
LeeHanSol <hslee4954@gmail.com>
2024-08-28 22:34:22 +0900

    이미지 로그인 제외한 css,js 캡슐화

--- src/main/java/com/busanit501/springproject3/lhs/config/CustomServletConfig.java
+++ src/main/java/com/busanit501/springproject3/lhs/config/CustomServletConfig.java
@@ -15,11 +15,11 @@ public class CustomServletConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("lhs/js/**")
-                .addResourceLocations("classpath:/static/lhs/js/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
 
         registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/lhs/css/");
+                .addResourceLocations("classpath:/static/css/");
 
         registry.addResourceHandler("/fonts/**")
                 .addResourceLocations("classpath:/static/fonts/");
@@ -32,7 +32,12 @@ public class CustomServletConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/images/item/**")
                 .addResourceLocations(uploadPath);
+
+        // lhs 경로 설정
+        registry.addResourceHandler("/lhs/js/**")
+                .addResourceLocations("classpath:/static/lhs/js/");
+        registry.addResourceHandler("/lhs/css/**")
+                .addResourceLocations("classpath:/static/lhs/css/");
+
     }
-
-
 }
